09d4838df01d9b9faff63476befd4959d66f010c
pedrxssss <pedro.sousa071100@gmail.com>
2023-10-05 16:50:07 -0300

    Alteração no Javascript: Divisivel por 2, 3 e 4

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 //Inputs de notas e media
 let cxNota1 = document.querySelector('#nota1')
 let cxNota2 = document.querySelector('#nota2')
+let cxNota3 = document.querySelector('#nota3')
+let cxNota4 = document.querySelector('#nota4')
 let cxMedia = document.querySelector('#media')
 
 //Botoes de calcular e limpar
@@ -17,20 +19,30 @@ let form = document.querySelector('form')
 /* Código funcional */
 
 //Calculo da média
-function calcularMedia(n1, n2) {
+function calcularMediaPorQuatro(n1, n2, n3, n4) {
+    return (n1 + n2 + n3 + n4) / 4
+}
+
+function calcularMediaPorTres(n1, n2, n3) {
+    return (n1 + n2 + n3) / 3
+}
+
+function calcularMediaPorDois(n1, n2) {
     return (n1 + n2) / 2
 }
+
+
 
 //Definicao da situacao final com base na media
 function situacaoFinal(mediaFinal) {
     let situacaoFinal = ''
 
     if (mediaFinal > 6) {
-       situacaoFinal = 'Aprovado(a)'
+        situacaoFinal = 'Aprovado(a)!'
     } else if (mediaFinal < 3.5) {
-       situacaoFinal = 'Reprovado(a)'
+        situacaoFinal = 'Reprovado(a)!'
     } else {
-       situacaoFinal = 'Recuperação'
+        situacaoFinal = 'Recuperação!'
     }
 
     return situacaoFinal
@@ -42,21 +54,22 @@ function formatarSituacao(situacaoFinal) {
     console.log(`Situação final ${situacaoFinal}`)
 
     switch (situacaoFinal) {
-        case 'Aprovado(a)':
+        case 'Aprovado(a)!':
             cxSit.classList.remove('reprovado')
             cxSit.classList.remove('recuperacao')
             cxSit.classList.add('aprovado')
             console.log('Adicionando classe de aprovado.')
             break
 
-        case 'Reprovado(a)':
+        case 'Reprovado(a)!':
+
             cxSit.classList.remove('aprovado')
             cxSit.classList.remove('recuperacao')
             cxSit.classList.add('reprovado')
             console.log('Adicionando classe de reprovado.')
             break
 
-        case 'Recuperação':
+        case 'Recuperação!':
             cxSit.classList.remove('reprovado')
             cxSit.classList.remove('aprovado')
             cxSit.classList.add('recuperacao')
@@ -70,22 +83,24 @@ function formatarSituacao(situacaoFinal) {
 function validarNumero(n) {
     let num1 = parseFloat(cxNota1.value)
     let num2 = parseFloat(cxNota2.value)
+    let num3 = parseFloat(cxNota3.value)
+    let num4 = parseFloat(cxNota4.value)
 
-    if (num1 < 0 || num1 > 10 || num2 < 0 || num2 > 10) {
+    if (num1 < 0 || num1 > 10 || num2 < 0 || num2 > 10 || num3 < 0 || num3 > 10 || num4 < 0 || num4 > 10) {
         form.reset()
         aviso.textContent = 'Digite uma nota entre 0.0 e 10.0'
         aviso.classList.add('alerta')
         setTimeout(function () {
             aviso.textContent = ''
             aviso.classList.remove('alerta')
-        }, 2000)
+        }, 3000)
     }
 }
 
 //Calculando a media apos click do botao de calcular
 
 //PreventDefault do btnCalcular
-btnCalcular.addEventListener('click', function(e) {
+btnCalcular.addEventListener('click', function (e) {
     //Pegar valor que esta dentro das caixas
     //Usar metodo parseFloat para converter string para float.
     //Prevent Default do btnCalcular
@@ -94,14 +109,20 @@ btnCalcular.addEventListener('click', function(e) {
     //Valores das caixas
     let nota1 = parseFloat(cxNota1.value)
     let nota2 = parseFloat(cxNota2.value)
-    let media = calcularMedia(nota1, nota2)
-
+    let nota3 = parseFloat(cxNota3.value)
+    let nota4 = parseFloat(cxNota4.value)
+    let media = calcularMediaPorQuatro(nota1, nota2, nota3, nota4)
+        || calcularMediaPorTres(nota1, nota2, nota3)
+        || calcularMediaPorDois(nota1, nota2)
+        
     console.log(nota1)
     console.log(nota2)
+    console.log(nota3)
+    console.log(nota4)
     console.log(media)
 
     //
-    if(isNaN(media) || media < 0) {
+    if (isNaN(media) || media < 0) {
         console.log('Não é um  número.')
         cxSit = ''
     } else {
@@ -112,7 +133,7 @@ btnCalcular.addEventListener('click', function(e) {
 })
 
 //Botão de limpar tirar as classes dos Sit
-btnLimpar.addEventListener('click', function(){
+btnLimpar.addEventListener('click', function () {
     cxSit.classList.remove('aprovado')
     cxSit.classList.remove('reprovado')
     cxSit.classList.remove('recuperacao')
